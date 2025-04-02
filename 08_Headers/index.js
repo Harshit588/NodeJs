8ca111e3db4fb45ddbfd67b083e11406eb307bef
Harshit Arya <123058771+Harshit588@users.noreply.github.com>
2025-04-02 11:29:31 +0000

    headers in Express

--- 08_Headers/index.js
+++ 08_Headers/index.js
@@ -2,11 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-
-
 app.get('/', (req, res) => {
-    console.log(req.headers);
-    res.setHeader('X-myName', 'Harshit')
+    console.log(req.headers); // print the coming custom header 
+    res.setHeader('X-myName', 'Harshit') // Send the header in response
     res.send('Hello World!')
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
