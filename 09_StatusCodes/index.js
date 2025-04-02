c01c09570f883c2d147d5da5e35a325beb7bfbd9
Harshit Arya <123058771+Harshit588@users.noreply.github.com>
2025-04-02 18:40:43 +0000

    Status Code in Express

--- 09_StatusCodes/index.js
+++ 09_StatusCodes/index.js
@@ -2,12 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({ extended: false })); // Middleware to parse URL-encoded data
+
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!')
 })
 
 app.post('/users', (req, res) => {
-    res.status(201).send({ 'status': "Success" })
+    const body = req.body;
+    if (!body || !body.username || !body.password) {
+        res.status(400).send({ "Status": "Not All Fields Revieced" }) //BAD REQUEST
+    } else {
+        console.log(body.username, " :: ", body.password);
+        res.status(201).send({ 'status': "Success" }) // CREATED
+    }
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))  
