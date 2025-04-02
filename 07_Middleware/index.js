5dace68bed8d7a809379cb3b0e994475cb2f6af4
Harshit Arya <123058771+Harshit588@users.noreply.github.com>
2025-04-02 10:30:49 +0000

    Middleware in ExpressJs

--- 07_Middleware/index.js
+++ 07_Middleware/index.js
@@ -15,4 +15,4 @@ app.use((request, response, next) => {
 })
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
